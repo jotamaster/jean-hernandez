ca2571b12d4e4a4324291fc560cdc49fe67db02f
jotamaster <j3anhernandez@gmail.com>
2020-06-16 10:57:12 -0400

    add slider images in personal page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import ImageGallery from 'react-image-gallery';
+
+
 
 function Index() {
+
+	const images = [
+		{
+		  original: 'https://i.pinimg.com/564x/55/3a/12/553a12d8ba7860bcddfb4186cf0c99dd.jpg',
+		  thumbnail: 'https://picsum.photos/id/1018/250/150/',
+		},
+		{
+		  original: 'https://66.media.tumblr.com/0331e318ec15f4515b9316a445125400/5e773b75fc80fa0d-9e/s640x960/1088c9370afbbb9f36454e16fc14df20e7e57083.png',
+		  thumbnail: 'https://picsum.photos/id/1015/250/150/',
+		}
+		
+	  ];
+
 	return (
 		<div className="personal">
 			<div className="sectionMenu">
@@ -13,7 +29,17 @@ function Index() {
 				</div>
 			</div>
 			<div className="sectionImages">
-				{/* <img src="https://i.pinimg.com/564x/55/3a/12/553a12d8ba7860bcddfb4186cf0c99dd.jpg" alt="personal gallery"/> */}
+				<ImageGallery 
+					showNav={false} 
+					size={'300px'}
+					showThumbnails={false}
+					items={images} 
+					showFullscreenButton={false}
+					showPlayButton={false}
+					autoPlay={false}
+					slideInterval={6000}
+					additionalClass={'mislide'}
+				/>
 			</div>
 			<div className="sectionDesc">
 				<div className="desc">
