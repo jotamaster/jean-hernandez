f8878bf81788c4b7ac9968e542d7f71227095530
jotamaster <j3anhernandez@gmail.com>
2020-06-19 19:37:31 -0400

    deploy mobile view

--- out/_next/static/runtime/webpack.js
+++ out/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "940a324cb7ff3edbbeca";
+/******/ 	var hotCurrentHash = "0ee2223d0c514fd8e10c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
