2b30c2be0e118216e4fd2e5fcea076930ba71e3a
jotamaster <j3anhernandez@gmail.com>
2020-06-22 21:08:58 -0400

    add structure blog menu

--- layout/blogLayout.js
+++ layout/blogLayout.js
@@ -3,7 +3,16 @@ import React, { Children } from 'react'
 function blogLayout({...props}){
 
   return(
-  <div>hola soy el otro layout{props.children}</div>
+  <div className="layoutblog">
+    <div className="navbar">
+      <div className="logo"></div>
+      <img src="img/menu.svg"></img>
+    </div>
+    <div className="menuMobile">
+    <a>Home</a>
+    <a>Blog</a>
+    </div>
+  </div>
   )
 }
 
